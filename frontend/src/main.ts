29bb9402201ca64d92dd79debf13820f94a4881e
Keyon Amirpanahi <keyon@keyoninfo.com>
2020-07-05 01:37:28 -0500

    Import game.ts in main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,20 +1,2 @@
 import './scss/main.scss'
-
-var playerContainer = document.getElementById("playercontainer")
-var scoreboard = <HTMLTableElement>document.getElementById("scoreboard")
-class Player {
-    name: string
-    score: number
-}
-
-function addPlayer(name:string) {
-    var newPlayer = document.createElement('div')
-    playerContainer.appendChild(newPlayer)
-
-    var row = scoreboard.insertRow(-1)
-    var nameCell = row.insertCell(0)
-    var scoreCell = row.insertCell(1)
-
-    nameCell.innerHTML = name
-    scoreCell.innerHTML = "0"
-}
+import 'game.ts'
